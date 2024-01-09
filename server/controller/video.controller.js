c5a3e458dd26a67496aca7ff561199696ea75825
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-10 00:01:29 +0530

    uploaded file

--- server/controller/video.controller.js
+++ server/controller/video.controller.js
@@ -1,20 +1,7 @@
-import multer from 'multer';
 import VideoModel from "../models/videoSchema.js";
-import UploadUtils from "../models/subtitleSchema.js";
 
-const storage = multer.diskStorage({
-  destination: UploadUtils.videoUploadPath,
-  filename: UploadUtils.videoFilename,
-});
-
-const upload = multer({ storage });
-
+// video upload controller
 export async function uploadVideo(req, res) {
-  upload.single('video')(req, res, async (err) => {
-    if (err) {
-      return res.status(500).json({ error: 'Error uploading video.' });
-    }
-
     const videoFile = req.file;
     const video = new VideoModel({ file: videoFile });
 
@@ -24,5 +11,5 @@ export async function uploadVideo(req, res) {
     } catch (error) {
       return res.status(500).json({ error: 'Error saving video to the database.' });
     }
-  });
+  
 }
--- server/routes/video.routes.js
+++ server/routes/video.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { uploadVideo } from "../controller/video.controller.js";
+import uploadFile from "../utils/uploadMiddleware.js"
 
 const Upload_Video = express.Router();
 
-Upload_Video.post('/upload', uploadVideo);
+Upload_Video.post('/upload', uploadFile, uploadVideo);
 
 export default Upload_Video;
