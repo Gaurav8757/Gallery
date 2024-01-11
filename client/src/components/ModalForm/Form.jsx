e4f02a36df00bd0f15b8ace77c04d6b7269ce2c4
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-12 03:07:54 +0530

    update

--- client/src/components/ModalForm/Form.jsx
+++ client/src/components/ModalForm/Form.jsx
@@ -123,7 +123,7 @@ const Form = () => {
           {uploadProgress > 0 && uploadProgress <= 100 && (
             <div className="flex justify-center items-center mt-4">
               <span className='text-white mr-2'>Uploading Video:</span>
-              <div className='w-10 sm:w-10 md:w-16 lg:w-20 xl:w-20 h-10  h-10 sm:h-10 md:h-16 lg:h-20 xl:h-20'>
+              <div className='w-10 sm:w-10 md:w-16 lg:w-20 xl:w-20  h-10 sm:h-10 md:h-16 lg:h-20 xl:h-20'>
                 <CircularProgressbar styles={buildStyles({
                   textColor: '#3CD79F ', // Text color
                   pathColor: '#3CD70A ', // Circular progress bar color
